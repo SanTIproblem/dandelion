987b96ac0661ae474bcec4d633b7d3863dffcc41
SanTIproblem <945992479@qq.com>
2022-02-10 11:50:10 +0800

    modify templates: account & portal

--- dandelion/settings.py
+++ dandelion/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'dandelion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dandelion',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
